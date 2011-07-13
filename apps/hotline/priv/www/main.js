9eac796f76b63ac15e2cde484f9c3d0746521e6f
James Wheare <james@wheare.org>
2011-07-13 18:19:07 +0100

    Modulise/comments/change function name

--- apps/hotline/priv/www/main.js
+++ apps/hotline/priv/www/main.js
@@ -1,23 +1,31 @@
-var ws = new WebSocket("ws://" + window.location.host);
-ws.onopen = function (event) {
-    console.info('WebSocket open', event);
-};
-ws.onmessage = function (event) {
-    try {
-        var message = JSON.parse(event.data);
-    } catch (exception) {
-        console.error('invalid JSON', event.data, exception);
-        return;
-    }
-    handle_message(message);
-};
-ws.onclose = function (event) {
-    console.info('WebSocket closed', event);
-};
-ws.onerror = function (event) {
-    console.warn('WebSocket error', event);
-};
-function append_scroll (message, text) {
+(function () {
+
+// Init socket
+function initSocket() {
+    var ws = new WebSocket("ws://" + window.location.host);
+    ws.onopen = function (event) {
+        console.info('WebSocket open', event);
+    };
+    ws.onmessage = function (event) {
+        try {
+            var message = JSON.parse(event.data);
+        } catch (exception) {
+            console.error('invalid JSON', event.data, exception);
+            return;
+        }
+        handle_message(message);
+    };
+    ws.onclose = function (event) {
+        console.info('WebSocket closed', event);
+    };
+    ws.onerror = function (event) {
+        console.warn('WebSocket error', event);
+    };
+    return ws;
+}
+
+// Write to scroll
+function writeScroll (message, text) {
     var scroll = $('#scroll');
     var scrollBottom = scroll.scrollTop() + scroll.height();
     var scrolledFromBottom = scroll.prop('scrollHeight') - scrollBottom;
@@ -27,6 +35,8 @@ function append_scroll (message, text) {
         scroll.scrollTop(scroll.prop('scrollHeight'));
     }
 }
+
+// Message handlers
 function handle_message (message) {
     switch (message.type) {
     case "idle":
@@ -34,34 +44,34 @@ function handle_message (message) {
         break;
     case "handshake":
         $('title').text(message.hostname + ' | Hotline');
-        append_scroll(message, "Handshaking…");
+        writeScroll(message, "Handshaking…");
         break;
     case "login":
-        append_scroll(message, "Logging in…");
+        writeScroll(message, "Logging in…");
         break;
     case "logged_in":
-        append_scroll(message, "Logged in.");
+        writeScroll(message, "Logged in.");
         break;
     case "chat_msg":
-        append_scroll(message, message.msg);
+        writeScroll(message, message.msg);
         break;
     case "server_msg":
-        append_scroll(message, '[' + message.from + '] ' + message.msg);
+        writeScroll(message, '[' + message.from + '] ' + message.msg);
         break;
     case "get_msgs":
         $('#news').text(message.messages.replace(/\r\n/g, '\n').replace(/\r/g, '\n'));
         break;
     case "socket_closed":
-        append_scroll(message, "Disconnected");
+        writeScroll(message, "Disconnected");
         break;
     case "user_joined":
-        append_scroll(message,  '→ ' + message.user.nick + ' joined');
+        writeScroll(message,  '→ ' + message.user.nick + ' joined');
         break;
     case "user_left":
-        append_scroll(message,  '← ' + message.user.nick + ' left');
+        writeScroll(message,  '← ' + message.user.nick + ' left');
         break;
     case "user_nick_change":
-        append_scroll(message, message.old_nick + ' → ' + message.user.nick);
+        writeScroll(message, message.old_nick + ' → ' + message.user.nick);
         break;
     case "user_name_list":
         var memberList = $('<ul>');
@@ -82,8 +92,9 @@ function handle_message (message) {
     }
 }
 
+// Socket API methods
 function send (message) {
-    ws.send(JSON.stringify(message));
+    SOCKET.send(JSON.stringify(message));
 }
 function chat (text, emote) {
     send({
@@ -93,6 +104,7 @@ function chat (text, emote) {
     });
 }
 
+// Input key handler
 $('#inputBox')
     .keydown(function (e) {
         switch (e.keyCode) {
@@ -106,3 +118,8 @@ $('#inputBox')
         }
     })
     .focus();
+
+// GO!
+SOCKET = initSocket();
+
+})();
