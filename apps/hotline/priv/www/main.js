82b2d937ac187d4c34a2cb2fd1a5ea700e6100e0
James Wheare <james@wheare.org>
2011-07-18 01:40:30 +0100

    .message -> .row

--- apps/hotline/priv/www/main.js
+++ apps/hotline/priv/www/main.js
@@ -37,7 +37,7 @@ function writeScroll (message, text, row) {
     var scrolledFromBottom = scroll.prop('scrollHeight') - scrollBottom;
     
     if (!row) {
-        row = $('<div class="message">');
+        row = $('<div class="row">');
         scroll.append(row);
     }
     var date = new Date(message.time * 1000);
@@ -60,7 +60,7 @@ function writeScroll (message, text, row) {
 }
 
 function getLastScroll () {
-    return $('#scroll div.message:last-child');
+    return $('#scroll div.row:last-child');
 }
 
 var User = Backbone.Model.extend({});
